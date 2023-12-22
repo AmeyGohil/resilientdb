1a39fce2de279a0c07588d9b8158fc37fc316d66
AmeyGohil <AmeyGohil@users.noreply.github.com>
2023-12-22 06:59:26 +0000

    deploy: 5f06d8b9b7ef5e421b9122ba2187beed015caf58

---
+++ navtreedata.js
@@ -0,0 +1,81 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "ResilientDB", "index.html", [
+    [ "Namespaces", "namespaces.html", [
+      [ "Namespace List", "namespaces.html", "namespaces_dup" ],
+      [ "Namespace Members", "namespacemembers.html", [
+        [ "All", "namespacemembers.html", null ],
+        [ "Functions", "namespacemembers_func.html", null ],
+        [ "Variables", "namespacemembers_vars.html", null ],
+        [ "Typedefs", "namespacemembers_type.html", null ],
+        [ "Enumerations", "namespacemembers_enum.html", null ],
+        [ "Enumerator", "namespacemembers_eval.html", null ]
+      ] ]
+    ] ],
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", "annotated_dup" ],
+      [ "Class Index", "classes.html", null ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", "functions_func" ],
+        [ "Variables", "functions_vars.html", null ],
+        [ "Typedefs", "functions_type.html", null ],
+        [ "Enumerations", "functions_enum.html", null ],
+        [ "Enumerator", "functions_eval.html", null ]
+      ] ]
+    ] ],
+    [ "Files", "files.html", [
+      [ "File List", "files.html", "files_dup" ],
+      [ "File Members", "globals.html", [
+        [ "All", "globals.html", null ],
+        [ "Functions", "globals_func.html", null ],
+        [ "Variables", "globals_vars.html", null ],
+        [ "Macros", "globals_defs.html", null ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"annotated.html",
+"d1/d89/classresdb_1_1PerformanceManager.html#ac532335c144ea148f52df8ad2822fda2",
+"d3/d51/kv__storage__test_8cpp.html#a679ba61b4152f83a46a4bb88b1cd7ca1",
+"d4/dac/classresdb_1_1MockNetChannel.html#a0cbb2349f4d390c3a506cc5749d8a9fe",
+"d6/da0/classresdb_1_1DuplicateManager.html#af8bd73e78c13634d84b201f8f0b28472",
+"d8/d22/classresdb_1_1ChainState.html#a8fca837b597b970808b5e09a536ed2e1",
+"d9/dc2/response__manager__test_8cpp.html#a25772ec6341de4facf7489566013b2fa",
+"da/dfa/classresdb_1_1TransactionExecutor.html",
+"dc/db0/classresdb_1_1CheckPointManager.html#a227d132998f3ffdde9aef287a6956cdf",
+"dd/dd7/address__manager__test_8cpp_source.html",
+"de/dda/mock__async__replica__client_8h_source.html",
+"functions_func_q.html"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
---
+++ search/pages_d.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['license_0',['license',['../d3/d2c/md__2github_2workspace_2node__modules_2badgen_2LICENSE.html',1,'LICENSE'],['../d2/ddb/md__2github_2workspace_2node__modules_2balanced-match_2LICENSE.html',1,'LICENSE']]],
+  ['lodash_20difference_20v4_205_200_1',['lodash.difference v4.5.0',['../dc/d57/md__2github_2workspace_2node__modules_2lodash_8difference_2README.html',1,'']]],
+  ['lodash_20union_20v4_206_200_2',['lodash.union v4.6.0',['../d8/d90/md__2github_2workspace_2node__modules_2lodash_8union_2README.html',1,'']]],
+  ['log_3',['Change Log',['../d9/d7c/md__2github_2workspace_2CHANGELOG.html',1,'']]]
+];
